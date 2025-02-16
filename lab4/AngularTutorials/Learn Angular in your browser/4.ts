de3ad296750c2da942ecfc377b32b57a94bf3f64
SanzharTest <157012984+sanzhikbogues@users.noreply.github.com>
2025-02-16 22:45:45 +0500

    add lab4

---
+++ lab4/AngularTutorials/Learn Angular in your browser/4.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  @if(isServerRunning){
+  <span>Yes, the server is running</span>
+  }@else{
+  <span>No, the server is not running</span>
+  }
+    
+  `,
+})
+export class AppComponent {
+  // add the boolean property here
+  isServerRunning=true;
+}
