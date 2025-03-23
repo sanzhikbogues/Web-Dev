23c6d8369997278506a84499098a27c6c26046bf
SanzharTest <157012984+sanzhikbogues@users.noreply.github.com>
2025-03-23 11:21:38 +0500

    Add files via upload

---
+++ lab7/myproject/task1/codingbat/list1/11.py
@@ -0,0 +1,7 @@
+def sum2(nums):
+    return sum(nums[:2])
+print(sum2([1, 2, 3]))      
+print(sum2([1, 1]))         
+print(sum2([1, 1, 1, 1]))   
+print(sum2([]))             
+print(sum2([5]))            
